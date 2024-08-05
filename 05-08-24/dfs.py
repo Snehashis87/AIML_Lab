ba06933c7987e532f9a0aad3ffd75b28f888a26a
Snehashis Mandal <121809988+Snehashis87@users.noreply.github.com>
2024-08-05 12:44:35 +0530

    Add files via upload

---
+++ 05-08-24/dfs.py
@@ -0,0 +1,32 @@
+Tree={
+    1:[2,7,8],
+    2:[3,6],
+    3:[4,5],
+    4:[],
+    5:[],
+    6:[],
+    7:[],
+    8:[9,12],
+    9:[10,11],
+    12:[],
+    10:[],
+    11:[]
+}
+def dfs_recursive(node, visited=None):
+    if visited is None:
+        visited = set() 
+    if len(visited)==7:
+        return visited
+    else :visited.add(node)
+    print(node, end=' ')  
+
+    for neighbor in Tree.get(node, []):
+        if neighbor not in visited:
+            dfs_recursive(neighbor, visited)
+    
+    return visited
+
+start_node = 1
+print("DFS Traversal Order (Recursive):")
+dfs_recursive(start_node)
+print() 
